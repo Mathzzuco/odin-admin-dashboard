d05babc798a81cf2978cd6a442e230a294beed5a
Mathzzuco <matheusmazzuco05@gmail.com>
2025-02-04 21:05:54 -0300

    add ability to create projects by clicking the 'NEW' button

--- js/script.js
+++ js/script.js
@@ -12,3 +12,46 @@ function changeIcon() {
     ringBell.style.display = "block";
   }
 }
+
+const button = document.querySelector(".test");
+button.addEventListener("click", () => {
+
+  const titleText = prompt("Write an title for your project!");
+  const descriptionText = prompt("Write an description for your project!");
+
+  if (titleText && descriptionText) {
+
+    const titleElement = document.createElement("h4");
+    const descriptionElement = document.createElement("p");
+
+    titleElement.innerHTML = titleText;
+    descriptionElement.innerHTML = descriptionText;
+
+    createProject(titleElement, descriptionElement);
+  }
+});
+
+function createProject(title, description) {
+
+  const projectWrapper = document.createElement("div");
+  projectWrapper.classList.add("project-wrapper");
+
+  const project = document.createElement("div");
+  project.classList.add("project");
+
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("text-container");
+
+  const a = document.createElement("a");
+  a.setAttribute("href", ".");
+
+  document.querySelector(".projects").prepend(projectWrapper);
+  projectWrapper.appendChild(project);
+  project.appendChild(textContainer);
+  textContainer.appendChild(a);
+
+  a.appendChild(title);
+  textContainer.appendChild(description);
+
+  console.log(a);
+}
